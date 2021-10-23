4b4961f37f7427c1a7e4484a451c20a1bc07d7ed
hlbigham <hlynnbigham@gmail.com>
2021-10-23 09:33:43 -0400

    V1
    Files from Week 17 to Update

---
+++ lynnfaiola.js
@@ -0,0 +1,20 @@
+/*accordion functionality*/
+var acc = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  };
+}
+
+//Card Flip Click
+$(document).on("click", ".flip-card", function () {
+    $(this).toggleClass('hover');
+});
+
